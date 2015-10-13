700eeaf05eb8980116b26174f11a9920fcbe8708
Robin-Manuel Thiel <robin-manuel.thiel@symanto.net>
2015-10-13 14:40:08 +0200

    Initial commit

---
+++ NavigationDrawerTest/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Support.Design.Widget;
+
+namespace NavigationDrawerTest
+{
+	[Activity(Label = "NavigationDrawer Test", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : AppCompatActivity
+	{
+		DrawerLayout drawerLayout;
+
+		protected override void OnCreate(Bundle bundle)
+		{			
+			base.OnCreate(bundle);
+
+			// Create UI
+			SetContentView(Resource.Layout.Main);
+			drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+			// Init toolbar
+			var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+			SetSupportActionBar(toolbar);	
+
+			// Attach item selected handler to navigation view
+			var navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+			navigationView.NavigationItemSelected += NavigationView_NavigationItemSelected;
+
+			// Create ActionBarDrawerToggle button and add it to the toolbar
+			var drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, Resource.String.open_drawer, Resource.String.close_drawer);
+			drawerLayout.SetDrawerListener(drawerToggle);
+			drawerToggle.SyncState();
+		}
+
+		void NavigationView_NavigationItemSelected(object sender, NavigationView.NavigationItemSelectedEventArgs e)
+		{
+			Toast.MakeText(this, e.MenuItem.TitleFormatted + " clicked.", ToastLength.Short).Show();
+			drawerLayout.CloseDrawers();
+		}
+	}
+}
